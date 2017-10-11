fe8722c43dac235b34e48a89c665dc71c4a44847
nievesalvin <alvinn4392@hstat.org>
2017-10-11 23:49:09 +0000

    lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1,num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(phrase="I'm right and you are wrong!")
+  phrase
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(default="cheese")
+    default
 end
 
-def whisper
-    
+def whisper(arguement)
+    arguement.downcase
 end
